a1ff2062859ac9b968768f738b856b3fe5fd6174
JdS161 <a.vasiltschenko@gmail.com>
2019-02-08 15:15:15 +0300

    Task is finished

--- Intro_to_CS_and_Prog_using_Python/secret_number.py
+++ Intro_to_CS_and_Prog_using_Python/secret_number.py
@@ -6,16 +6,30 @@ Created on Wed Feb  6 14:18:51 2019
 @author: jds
 """
 
-x = int(input('Please think of a number between 0 and 100!' ))
-epsilon = 0.01
-low = 0.0
-high = x
-ans = (high+low)/2.0
+print('Please think of a number between 0 and 100!' )
 
-while abs(ans**2 -x) >= epsilon:
+low = 0.0
+high = 100.0
+ans = 0
+result = ''
+
+while result != 'c':
+    ans = (high+low)//2.0
+    result = input ('Is your secret number ' + str(ans) + "? \nEnter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
+    if result == 'h':
+        high = ans    
+    elif result == 'l':
+        low = ans
+    elif  result == 'c':
+        print ('Game over. Your secret number was:' + str(ans))
+        break
+    else:
+        print ('Sorry, I did not understand your input.')
+        continue
     
     
-    Enter 'h' to indicate the guess is too high. 
+"""  
+  Enter 'h' to indicate the guess is too high. 
     \nEnter 'l' to indicate the guess is too low. 
     \nEnter 'c' to indicate I guessed correctly.
     
@@ -23,4 +37,5 @@ while abs(ans**2 -x) >= epsilon:
     Sorry, I did not understand your input.
     
     Game over. Your secret number was:
-    
+
+        """
